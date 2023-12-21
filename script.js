b98ecf2d2affee6c38c283d4838a21ff3a619099
Dhruv Varshney <144780592+dhruv996@users.noreply.github.com>
2023-12-21 14:39:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const makeChange = (c) => {
-  // your name here
-};
+function makeChange(amount) {
+  const quarters = Math.floor(amount / 25);
+  const dimes = Math.floor((amount % 25) / 10);
+  const nickels = Math.floor(((amount % 25) % 10) / 5);
+  const pennies = ((amount % 25) % 10) % 5;
 
-// Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+  return { q: quarters, d: dimes, n: nickels, p: pennies };
+}
+
+function calculateChange() {
+  const amount = parseInt(document.getElementById('amount').value);
+  const result = makeChange(amount);
+
+  document.getElementById('result').innerText = `Change breakdown: ${JSON.stringify(result)}`;
+}
+
